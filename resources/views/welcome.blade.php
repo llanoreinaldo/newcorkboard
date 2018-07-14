b5ba073cca488e77ee341fd0055bb6a47e80c3e8
Reinaldo Llano, Jr <llanoreinaldo@gmail.com>
2018-07-14 11:26:26 -0400

    fix fonts links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Corkboard</title>
+        
+        <!-- Style Sheets -->
         <link href="{{mix('css/app.css')}}" rel="stylesheet" type="text/css">
         <link href="{{asset('css/home.css')}}" rel="stylesheet" type="text/css">
 
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+                
         <!-- CSRF Token -->
          <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -20,9 +25,10 @@
     <div id="pimg2"></div>
     <div id="section2"></div>
     <div id="pimg3"></div>
-    <div id="section3"></div>
     <div id="inviteModal"></div>
     <div id="errorModal"></div>
+    <div id="section3"></div>
+    <div id="pimg1"></div>
 
         <script src="{{mix('js/app.js')}}" ></script>
     </body>
