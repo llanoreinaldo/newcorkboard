0d56ae83c3572e5291549f57e45139d63efb86de
Reinaldo Llano, Jr <llanoreinaldo@gmail.com>
2018-07-16 18:38:30 -0400

    changes to two new components

--- resources/assets/js/components/Board/Announcements.js
+++ resources/assets/js/components/Board/Announcements.js
@@ -1,32 +1,30 @@
-import React, { Component } from '../../../../../../../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react';
-import ReactDOM from '../../../../../../../../../../../AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-dom';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 export default class Announcements extends Component {
     render() {
         return (
 
-    {/*<!--ANNOUNCEMENTS-->*/}
-<div className="main">
-    <div className="card sidebar">
-        <div className="card-header" style="background:#001945;color:white;">
+        {/*<!--ANNOUNCEMENTS-->*/}
+
+                <div className="main">
+                    <div className="card sidebar">
+                       <div className="card-header" style={{background:"#001945"}} style={{color:white}}>
             Announcements
-            <i className="fas fa-plus-circle newAnn" data-toggle="modal" data-target="#announcementModal"></i>
+                            <i className="fas fa-plus-circle newAnn" data-toggle="modal" data-target="#announcementModal"></i>
+                        </div>
+                    <div className="annoucementCard-body">
+                    {{#each board:messages}}
+                    <p className="card-text" style={{paddingBottom:10}}>
+                        <i className="fas fa-dot-circle" style={{color:'#031244'}}></i>
+                        {{msg}} by
+                        <span id="author">{{author}}</span>
+                    </p>
+                    {{/each}}
+                    </div>
+                </div>
 
-
-        </div>
-
-        <div className="annoucementCard-body">
-            {{#each board.messages}}
-            <p className="card-text" style="padding-bottom:10px;">
-                <i className="fas fa-dot-circle" style="color:#031244;"></i>
-                {{msg}} by
-                <span id="author">{{author}}</span>
-            </p>
-            {{/each}}
-        </div>
-    </div>
-
-    <!--dropdown announcement-->
+    {/* <!--dropdown announcement--> */}
 
     <div className="card col-lg-12 col-md-4" id="dropAnnounce" style="background:#100F4E;color:white;">
 
@@ -34,20 +32,19 @@ export default class Announcements extends Component {
             Announcements
             <i className="fas fa-plus-circle newAnn" data-toggle="modal" data-target="#announcementModal"></i>
         </div>
-        <div className="card-body onPlusClose" style="overflow-y: scroll;
-position: absolute;
-height: 100%; width:95%;">
-            {{#each board.messages}}
-            <p className="card-text" style="padding-bottom:10px;">
-                <i className="fas fa-dot-circle" style="color:#031244;"></i>
+        <div className="card-body onPlusClose" style={{ overflowY: scroll}} style={{ position: absolute}} style={{ height:"100%"}} style={{width:'95%'}}>
+            {{#each board:messages}}
+            <p className="card-text" style={{paddingBottom: '10px'}}>
+                <i className="fas fa-dot-circle" style={{ color: '#031244'}}></i>
                 {{msg}} by
                 <span id="author">{{author}}</span>
             </p>
-            {{/each}} {{!--
-            <a href="#" className="btn btn-primary">Go somewhere</a> --}}
+            {{/each}} 
+            {/* {{!-- 
+            <a href="#" className="btn btn-primary">Go somewhere</a> --}} */}
         </div>
     </div>
-</div>
+
        
 );
 }
