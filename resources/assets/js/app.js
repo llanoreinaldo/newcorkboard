5f5b3041bedcff13c2573c22998ae59bed26892f
Reinaldo Llano, Jr <llanoreinaldo@gmail.com>
2018-07-16 19:12:19 -0400

    commit app js file changes handling imports

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,15 +12,6 @@ require("./bootstrap");
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 // import Main from "./components/Main";
-import Pimg1 from "./components/Pimg1";
-import Pimg2 from "./components/Pimg2";
-import Pimg3 from "./components/Pimg3";
-import Section1a from "./components/Section1a";
-import Section1na from "./components/Section1na";
-import Section2 from "./components/Section2";
-import Section3 from "./components/Section3";
-import InviteModal from "./components/InviteModal";
-import ErrorModal from "./components/ErrorModal";
 
 import Pimg1 from './components/Home/Pimg1';
 import Pimg2 from './components/Home/Pimg2';
@@ -31,12 +22,14 @@ import Section2 from './components/Home/Section2';
 import Section3 from './components/Home/Section3';
 import InviteModal from './components/Home/InviteModal';
 import ErrorModal from './components/Auth/ErrorModal';
-import RegisterModal from './components/Auth/RegisterModal';
+// import RegisterModal from './components/Auth/RegisterModal';
+// import LoginModal from './components/Auth/LoginModal'
 
-import Filters from '.components/Board/Filters';
-import Navbar from '.components/Board/Navbar';
-import Cards from '.components/Board/Cards';
-import Announcements from '.components/Board/Announcements'
+import Filters from './components/Board/Filters';
+import Navbar from './components/Board/Navbar';
+import Cards from './components/Board/Cards';
+import Announcements from './components/Board/Announcements'
+import BoardModals from './components/Board/BoardModals'
 
 
 require('./components/LoginModal')
